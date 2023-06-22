c40b1fcd23cbc39a517c6b202acc8382afae3955
SkyWilcots <wilcotssk@gmail.com>
2023-06-22 17:17:41 -0400

    Setting up initial values in database

---
+++ java/database/create script.sql
@@ -0,0 +1,103 @@
+INSERT INTO dimension(
+	dimension_id,
+	dimension_name,
+	dimension_code,
+	description
+)
+VALUES(
+	0,
+	'The Interdimensional Rift',
+	'000',
+	'The beginning, end, and intersection of all worlds. It''s so massive that stumbling in without a way through or back is probably a fate worse than death. We''ve not yet probed the full depths of the Rift, and I doubt we ever will in our lifetime, but there seems to be one creature that can navigate the void as though it has been there since its inception...'
+);
+
+INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'World of Cosmos',
+	'1-Z',
+	'Our planet, given form by Cosmos, and the crystals she created. In our current era, Onrac and Cornelia have made significant advances in technology, leading to great prosperity for all. However, the world has been in a state of decline and decay as Chaos seems to be returning. But as the prophecy goes, when the winds die, the sea languishes, and the earth rots, the four Warriors of Light shall come.'
+);
+
+INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'The Great Garden',
+	'2-A',
+	'Altan''s home world. According to information from him, there was once a despotic emperor named Mateus Palamecia, whose ruthlessness and tenacity almost allowed him to conquer the entire globe. That was a long time ago, it seems, as Altan claims the heroes responsible for the peace he lives under have long since died a peaceful death. It seems this world does not have crystals the way ours does.'
+);
+
+INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'World of Light',
+	'3-A',
+	'A world not too unlike our own, this land that floats along the clouds seems to carry with it the power of four crystals of light. Reports indicate that there is a counterbalance of energy equal to that of the crystals somewhere in this dimension. Is it possible this world has crystals of darkness as well? What does this say about the state of equilibrium in that world--or ours, for that matter?'
+);
+	
+	INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'Blue Planet',
+	'4-A',
+	'Another world that apparently has crystal energy signatures--16 of them. Four have been found on the earth''s surface, four more presumably under its stratum, and another eight seeming to come from the stars. Despite this abundance of crystal energy on this planet, the world seems to still be in an era not unlike ours during the Earth-Sky Wars.'
+);
+	
+	INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'Rift-Torn World',
+	'5-R',
+	'This dimension seems to be riddled with portals leading into the Interdimensional Rift, quite similar to the recent phenomenon we have been experiencing in recent months. However, unlike our relatively miniscule pockets, these portals have carved holes into entire continents, mountains and seas. Is this what awaits our world if they can''t be closed?'
+	);
+	
+	INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+		'World of Balance',
+		'6-A',
+		'A universe with neither crystals nor magic. One would think this world would be nearly uninhabitable, but humans do well enough with the power of steam technology. Apparently the world''s balance hinges on the counterbalancing of three strong energy readings hidden somewhere beneath the earth''s surface.'
+);
+
+	INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'Fount of Life',
+	'7-L',
+	'Reports indicate an extremely powerful energy signature buried within the planet''s core. It''s much like the energy readings given off by the crystals of our home world, yet singular, and massive. An interesting question was posed as to what technology would be required to harness all of that energy on this world.'
+	);
+	
+	
+	
+INSERT INTO dimension (
+	dimension_name,
+	dimension_code,
+	description)
+VALUES (
+	'Time-Bent Landscape',
+	'8-G',
+	'No accurate readings could be gathered from this dimension. Interestingly, remote recordings from probes seem to indicate time as being in constant chaotic flux. This has made return trips for our probes impossible at this time. Because of the potential risks the temporal instability presents, this world has been deemed a serious hazard for even our probes. Human travel will likely never be viable there.'
+	);
+	
+INSERT INTO dimension (
+	dimension_name,
+	dimension_code)
+VALUES (
+	'Gaia',
+	'9-A');
+	
+	SELECT * FROM dimension;
